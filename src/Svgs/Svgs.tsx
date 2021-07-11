c634c2d625c4b02d0aea0a2e1e33bbb98e78ba8a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-11 22:48:33 +0530

    add : header svgs

---
+++ src/Svgs/Svgs.tsx
@@ -0,0 +1,34 @@
+import { SVGProps } from "react-router/node_modules/@types/react";
+
+export function LogosGoogleKeep(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="2.5em" height="2.5em" viewBox="0 0 256 352" {...props}><path d="M232 352H24c-13.28 0-24-10.72-24-24V24C0 10.72 10.72 0 24 0h144l88 88v240c0 13.28-10.72 24-24 24z" fill="#FFBA00"></path><path fill="#FF9500" d="M168 0l88 88h-88z"></path><path d="M156 236v20h-56v-20h56zm-28.08-115.999l.78.006c28.323.46 51.14 23.56 51.14 51.993c0 18.56-9.72 34.8-24.32 44h-55.2C85.68 206.8 76 190.56 76 172c0-28.72 23.28-52 52-52l-.08.001z" fill="#FFF"></path></svg>
+  )
+}
+
+export function BxBxSearchAlt2(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg  color="gray" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392l.604.646l2.121-2.121l-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5s5 2.243 5 5s-2.243 5-5 5z" fill="currentColor"></path></svg>
+    )
+  }
+
+  
+export function AkarIconsCross(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg color="gray" width="1.4em" height="1.4em" viewBox="0 0 24 24" {...props}><g fill="none"><path d="M20 20L4 4m16 0L4 20" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path></g></svg>
+  )
+}
+
+
+export function IcOutlineWbSunny(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg color="gray"  width="1.6em" height="1.6em" viewBox="0 0 24 24" {...props}><path d="M6.76 4.84l-1.8-1.79l-1.41 1.41l1.79 1.79zM1 10.5h3v2H1zM11 .55h2V3.5h-2zm8.04 2.495l1.408 1.407l-1.79 1.79l-1.407-1.408zm-1.8 15.115l1.79 1.8l1.41-1.41l-1.8-1.79zM20 10.5h3v2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6s-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4s4 1.79 4 4s-1.79 4-4 4zm-1 4h2v2.95h-2zm-7.45-.96l1.41 1.41l1.79-1.8l-1.41-1.41z" fill="currentColor"></path></svg>
+  )
+}
+
+
+export function CarbonUserAvatar(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg color="gray"  width="1.6em" height="1.6em" viewBox="0 0 32 32" {...props}><path d="M16 8a5 5 0 1 0 5 5a5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3a3.003 3.003 0 0 1-3 3z" fill="currentColor"></path><path d="M16 2a14 14 0 1 0 14 14A14.016 14.016 0 0 0 16 2zm-6 24.377V25a3.003 3.003 0 0 1 3-3h6a3.003 3.003 0 0 1 3 3v1.377a11.899 11.899 0 0 1-12 0zm13.992-1.451A5.002 5.002 0 0 0 19 20h-6a5.002 5.002 0 0 0-4.992 4.926a12 12 0 1 1 15.985 0z" fill="currentColor"></path></svg>
+  )
+}
