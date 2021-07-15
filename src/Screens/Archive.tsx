5a6ea0896d5e48c0973d0f07453924287b37d00b
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-16 00:56:54 +0530

    add: onchange event

--- src/Screens/Archive.tsx
+++ src/Screens/Archive.tsx
@@ -22,16 +22,18 @@ return (
     <LeftNav />
     <div className="trash_cards">
       { state?.archive?.map(({title,description,label,id,color}:Notes)=>{
-      return <div className="card_div" style={{backgroundColor:color}}>
+      return <div key={id} className="card_div" style={{backgroundColor:color}}>
         <form>
           <div className="card_title_pin">
-            <input style={{backgroundColor:color}} className="card_title_input" value={title} placeholder="Title" type="text" />
+            <input style={{backgroundColor:color}} className="card_title_input" value={title} placeholder="Title"
+              type="text"
+              onChange={(e)=>dispatch({type:"CHANGE_ARCHIVED_NOTES_TITLE",payload:{newTitle:e.target.value,id:id}})}/>
             <PinArchivedNote onClick={()=>dispatch({type:"PIN_ARCHIVED_NOTE",payload:{id}})} pinText={"Pin archived Note"}/>
           </div>
           <br />
           <div className="card_text_box">
-            <textarea style={{backgroundColor:color}} cols={50} className="text_area" placeholder="Take a note..." name="text"
-              value={description}></textarea>
+            <textarea style={{backgroundColor:color}} cols={50} className="text_area" placeholder="Take a note..."
+              name="text" value={description}            onChange={(e)=>dispatch({type:"CHANGE_ARCHIVED_NOTES_DESCRIPTION",payload:{newDescription:e.target.value,id:id}})} ></textarea>
           </div>
           <div className="label">lololol</div>
           <div className="card_icons_btns">
