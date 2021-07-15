62d5155dee5e58fd72619219c56d411f813ea118
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-16 00:57:30 +0530

    add useState types

--- src/Context/GoogleKeepProvider.tsx
+++ src/Context/GoogleKeepProvider.tsx
@@ -1,19 +1,28 @@
-import { createContext,Dispatch, useContext, useReducer } from "react";
+import { createContext,Dispatch,SetStateAction, useContext, useReducer, useState } from "react";
 import { reducer ,initialState,ACTION} from "./reducer/googleKeepReducer";
 import {ReducerInitialState} from "../../src/Context/types"
 
 interface ContextType {
     state:ReducerInitialState;
     dispatch: Dispatch<ACTION>
+    title:string;
+    setDescription:Dispatch<SetStateAction<string>>;
+    setTitle:Dispatch<SetStateAction<string>>;
+    setLabel:Dispatch<SetStateAction<string>>;
+    description:string;
+    label:string;
 }
 
 const GoogleKeepContext = createContext({} as ContextType);
 
 export function GoogleKeepProvider({children}:any){
 
-    const [state,dispatch] = useReducer(reducer,initialState)
+    const [state,dispatch] = useReducer(reducer,initialState);
+    const [title,setTitle] = useState<string>('')
+    const [description,setDescription] = useState<string>('')
+    const [label,setLabel] = useState<string>('')
     return (
-        <GoogleKeepContext.Provider value={{state,dispatch}}>{children}</GoogleKeepContext.Provider>
+        <GoogleKeepContext.Provider value={{state,dispatch,title,setDescription,setTitle,description,label,setLabel}}>{children}</GoogleKeepContext.Provider>
     )
 }
 
