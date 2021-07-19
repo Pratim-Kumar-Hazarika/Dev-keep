9907456c24676d4296579c43377458f080dbf401
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-20 01:21:06 +0530

    add :login form

---
+++ src/Components/Login/LoginForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Login.css"
+export default function LoginForm() {
+    return (<> <div className="login_content">
+        <div className="login_heading">
+            <span
+                style={{
+                fontWeight: "bold"
+            }}>Login</span>
+        </div>
+        <div className="email_div">
+            <div>Email</div>
+            <input className="email_input" type="text" name="" id=""/>
+        </div>
+        <div className="password_div">
+            <div>Password</div>
+            <input className="password_input" type="text" name="" id=""/>
+        </div>
+        <div>
+            <button className="login_btn">Login</button>
+            <div className="dont_have_account_div">
+                <span>Don't have an account ?</span>
+                <span className="signup_text">
+                    Signup</span>
+            </div>
+        </div>
+    </div>
+    </>
+    )
+}
