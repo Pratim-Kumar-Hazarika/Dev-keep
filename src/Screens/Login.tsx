c8401e9f5f5d27886c9cd6f8e975e6c8c23e0b9b
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-21 01:28:33 +0530

    add : hero image

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -1,7 +1,7 @@
 import "../login.css"
 import LoginForm from '../Components/Login/LoginForm';
 import { LogosGoogleKeep } from '../Components/Reusable/HeroLogo';
-
+import {ReactComponent as HeroImage} from "../Svgs/notes2.svg"
 export default function Login() {
     return (
         <div className="login">
@@ -9,8 +9,8 @@ export default function Login() {
                 <div className="quote">
                     <span className="note_quote">Relax And Take Notes</span>
                 </div>
-                <div className="hero_image">
-                    <LogosGoogleKeep/>
+                <div className="hero_image1">
+                    <HeroImage/>
                 </div>
             </div>
             <div className="login_form">
