1b1c9d89584e5aa3b7b494a3a0b21f004c3a6695
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-07-21 01:28:04 +0530

    add : signup page

---
+++ src/Screens/Signup.tsx
@@ -0,0 +1,21 @@
+import "../login.css"
+import { LogosGoogleKeep } from '../Components/Reusable/HeroLogo';
+import SignUpForm from "../Components/Signup/SignupForm";
+import {ReactComponent as HeroImage} from "../Svgs/notes.svg"
+export default function Signup() {
+    return (
+        <div className="login">
+            <div className="login_text">
+                <div className="quote">
+                    <span className="note_quote mt_10">Relax And Take Notes</span>
+                </div>
+                <div className="hero_image">
+                    <HeroImage/>
+                </div>
+            </div>
+            <div className="login_form">
+               <SignUpForm/>
+            </div>
+        </div>
+    )
+}
