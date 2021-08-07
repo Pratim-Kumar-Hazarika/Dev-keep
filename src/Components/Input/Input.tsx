3e943cf3e5ef6e22e14257b59a46e9641afb74e4
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 02:03:40 +0530

    refactor

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,50 +1,22 @@
 import { useState } from 'react';
 import { useGoogleKeep } from '../../Context/GoogleKeepProvider'
 import { colorsData } from '../../Context/reducer/colors';
+import { addNoteHandler } from '../../Context/utils/addNoteHandler';
+import { archiveClickHandler } from '../../Context/utils/archiveNoteClickHandler';
+import { pinClickHandler } from '../../Context/utils/PinClickHandler';
 import { BxBxImageAlt} from '../../Svgs/Svgs'
 import { ArchiveNote } from '../Reusable/ArchiveNote';
 import { ChangeColor } from '../Reusable/ChangeColor';
 import { PinNoteFromInput } from '../Reusable/PinNoteFromInput';
-import { VerticalDots } from '../Reusable/VerticalDots';
 import "./Input.css";
 function Input() {
-const {title,setDescription,setTitle,description,label,setLabel,setBgColor,bgColor,dispatch,state} = useGoogleKeep()
-
-function addNoteHandler(e:any){
-e.preventDefault()
-    if(title || description !== ''){
-    dispatch({type:"ADD_NOTE",payload:{id:Math.random() ,title:title,description:description,label:[label],color:bgColor}})
-    setTitle("");
-    setDescription('')
-    setBgColor("")
-    setLabel("")
-    }
-}
-console.log("the state is",state)
-
-function archiveClickHandler(){
-    if(title || description !== ''){
-    dispatch({type:"ARCHIVE_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[label],color:bgColor}})
-    setTitle("");
-    setDescription('')
-    setBgColor("")
-    }
-}
-function pinClickHandler(){
-    if(title || description !==''){
-    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[label],color:bgColor}})
-    setTitle("");
-    setDescription('')
-    setBgColor("")
-    }
-}
+const {title,setDescription,setTitle,description,setLabel,setBgColor,bgColor,dispatch} = useGoogleKeep()
 const [overflow,setOverflow] = useState("");
-const [height,setHeight] = useState("")
+const [height,setHeight] = useState("");
 function formExtendClickHandler(){
     setOverflow("visible ")
     setHeight("7rem")
 }
-const [newId,setNewId] = useState<number>(0)
 
 return (
 <div className="input_div" style={{backgroundColor:bgColor,overflow:overflow,height:height}} onClick={formExtendClickHandler}>
@@ -52,7 +24,7 @@ return (
         <div className="title_pin">
             <input  style={{backgroundColor:bgColor}} className="title_input" value={title} placeholder={height ?"Title":"Take a note..."}
                 type="text" onChange={(e)=>setTitle(e.target.value)}/>
-           {height  && <PinNoteFromInput onClick={pinClickHandler} /> } 
+           {height  && <PinNoteFromInput onClick={(e)=>pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel})} /> } 
         </div>
         <br />
         <div className="text_box">
@@ -67,7 +39,7 @@ return (
                     <div className="color_divs ">
                         {
                         colorsData.map((color:any)=>(
-                        <div className="circle tooltip" style={{backgroundColor:color.color}} onClick={()=>
+                        <div key={color.color} className="circle tooltip" style={{backgroundColor:color.color}} onClick={()=>
                             setBgColor(color.color)} >
                             <span className="tooltiptext">{color.name}</span>
                         </div>
@@ -76,12 +48,11 @@ return (
                     </div>
                 </div>
                 <BxBxImageAlt />
-                <ArchiveNote onClick={archiveClickHandler } />
-              
+                <ArchiveNote onClick={(e)=>archiveClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel}) } />             
             </div>
             <div className="text_box_btns">
-                <button className="addNote_btn" onClick={addNoteHandler} type="submit">Add Note</button>
-           
+                <button className="addNote_btn" onClick={(e)=>addNoteHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel})} type="submit">Add Note</button>
+                {/* <VerticalDots noteId={Math.random()}/> */}
             </div>
         </div>
     </form>
