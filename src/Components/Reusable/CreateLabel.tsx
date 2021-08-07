c26edaa81cf3ff3bf22cefa10055571bc1387138
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 02:04:43 +0530

    fix: imports

--- src/Components/Reusable/CreateLabel.tsx
+++ src/Components/Reusable/CreateLabel.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { TeenyiconsTickOutline } from '../EditLabel/EditLabel'
+import { TeenyiconsTickOutline } from '../Svgs/Svg'
+
 
 export const CreateLabel: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
