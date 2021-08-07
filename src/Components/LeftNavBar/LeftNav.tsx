be3b21e8f9b583e38b47e0d21142bc0396d2b1f7
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 02:04:04 +0530

    update : set visibility

--- src/Components/LeftNavBar/LeftNav.tsx
+++ src/Components/LeftNavBar/LeftNav.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { MdiArchiveArrowDownOutline, MdiLightbulbOutline, MdiLightPencil, MdiTrashCanOutline } from '../../Svgs/Svgs'
 import "./LeftNav.css"
 import {Link} from "react-router-dom"
+import { useGoogleKeep } from '../../Context/GoogleKeepProvider'
 export default function LeftNav() {
+    const {setShowLabelModel} = useGoogleKeep();
+    function labelHandler(){
+        setShowLabelModel("visible")
+    }
     return (
         <div className="left_nav" >
          <Link to="/" className="link" >
@@ -17,9 +22,9 @@ export default function LeftNav() {
                     <span className="left_nav_icons_text">Archive</span>
             </div>
         </Link>
-            <div className="left_nav_contents edit_labels">
+            <div className="left_nav_contents edit_labels" onClick={labelHandler}>
                 <MdiLightPencil className="svg"/>
-                <span className="left_nav_icons_text">Edit Lables</span>
+                <span className="left_nav_icons_text" >Edit Lables</span>
             </div>
         <Link to="/trash"  className="link">
             <div className="left_nav_contents trash">
