05bb1f3d9333a0a205b6a6eb8ec3199d4cfa2178
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 02:05:23 +0530

    create svg.tsx

---
+++ src/Components/Svgs/Svg.tsx
@@ -0,0 +1,29 @@
+import {SVGProps} from "react-router/node_modules/@types/react";
+export function MdiPencil(props : SVGProps < SVGSVGElement >) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25z"
+                fill="currentColor"></path>
+        </svg>
+    )
+}
+export function DashiconsTrash(props : SVGProps < SVGSVGElement >) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 20 20" {...props}>
+            <path
+                d="M12 4h3c.6 0 1 .4 1 1v1H3V5c0-.6.5-1 1-1h3c.2-1.1 1.3-2 2.5-2s2.3.9 2.5 2zM8 4h3c-.2-.6-.9-1-1.5-1S8.2 3.4 8 4zM4 7h11l-.9 10.1c0 .5-.5.9-1 .9H5.9c-.5 0-.9-.4-1-.9L4 7z"
+                fill="currentColor"></path>
+        </svg>
+    )
+}
+
+export function TeenyiconsTickOutline(props : SVGProps < SVGSVGElement >) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 15 15" {...props}>
+            <g fill="none">
+                <path d="M1 7l4.5 4.5L14 3" stroke="currentColor" strokeLinecap="square"></path>
+            </g>
+        </svg>
+    )
+}
