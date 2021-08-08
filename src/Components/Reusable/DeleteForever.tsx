93b7cc42ec410ff7a0895aa7b1ba9a715af53d60
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 16:41:55 +0530

    update : change delete icon svg

--- src/Components/Reusable/DeleteForever.tsx
+++ src/Components/Reusable/DeleteForever.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MdiTrashCanOutline } from '../../Svgs/Svgs'
+import { NotoV1Wastebasket } from '../Svgs/Svg'
 
 export const DeleteForever: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
         <div className="tooltip" onClick={onClick}>
-        <MdiTrashCanOutline />
+        <NotoV1Wastebasket />
         <span className="tooltiptext">Delete Forever</span>
     </div>
     )
