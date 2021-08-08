a859168e52c68f36e93f8a04f3b28c0dcd851f9d
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-09 00:08:36 +0530

    add : image prop

--- src/Context/utils/PinClickHandler.ts
+++ src/Context/utils/PinClickHandler.ts
@@ -1,10 +1,11 @@
 import { AddNoteHandler } from './addNoteHandler';
 
-export function pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel}:AddNoteHandler){
+export function pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource}:AddNoteHandler){
     if(title || description !==''){
-    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor}})
+    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,image:previewImage}})
     setTitle("");
     setDescription('')
     setBgColor("")
+    setPreviewImageSource("")
     }
 }
