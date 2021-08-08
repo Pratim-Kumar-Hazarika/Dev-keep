77cc6fd94c1237fbd50220de5c4acebc8a5aded2
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 16:41:12 +0530

    update : change image svg

--- src/Components/Reusable/AddImage.tsx
+++ src/Components/Reusable/AddImage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BxBxImageAlt } from '../../Svgs/Svgs'
+import { FxemojiFramewithpicture } from '../Svgs/Svg'
 
 function AddImage() {
 return (
 <div className="tooltip">
-    <BxBxImageAlt />
+    <FxemojiFramewithpicture />
     <span className="tooltiptext">Add image</span>
 </div>
 )
