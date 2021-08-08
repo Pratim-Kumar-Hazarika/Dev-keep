d061fe685d07619ddceffb34f2274a620a556d4e
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-09 00:07:51 +0530

    add : image

--- src/Screens/Trash.tsx
+++ src/Screens/Trash.tsx
@@ -1,3 +1,4 @@
+import EditLabel from '../Components/EditLabel/EditLabel'
 import Header from '../Components/Header/Header'
 import LeftNav from '../Components/LeftNavBar/LeftNav'
 import { DeleteForever } from '../Components/Reusable/DeleteForever'
@@ -6,23 +7,28 @@ import { useGoogleKeep } from '../Context/GoogleKeepProvider'
 import { Notes } from '../Context/types'
 
 export default function Trash() {
-const {state,dispatch} = useGoogleKeep()
+const {state,dispatch,keepOpacity} = useGoogleKeep()
 return (
 <>
     <Header />
-    <div style={{display:"flex"}}>
+    <EditLabel/>
+    <div style={{display:"flex",opacity:keepOpacity? "0.4":"1"}}>
         <LeftNav />
         <div className="trash_cards">
-            { state?.trash?.map(({title,description,label,id,color}:Notes)=>{
-            return <div key={id} className="card_div" style={{backgroundColor:color}}>
+            { state?.trash?.map(({title,description,label,id,color,image}:Notes)=>{
+            return <><div key={id} className="card_div" style={{backgroundColor:color}}>
+                 <div className="uplod_img_div">
+                <img className="uplod_img" src={image}/>
+            </div>
                 <span> {title}</span>
                 <p>{description}</p>
-                <div className="label">{label}</div>
+                <div className="label">"lolo"</div>
                 <div className="trash_card_icons">
                     <RestoreNote onClick={()=>dispatch({type:"RESTORE_NOTE",payload:{id}})}/>
                     <DeleteForever onClick={()=> dispatch({type:"DELETE_FOREVER",payload:{id}})}/>
                 </div>
             </div>
+            </>
             })}
         </div>
     </div>
