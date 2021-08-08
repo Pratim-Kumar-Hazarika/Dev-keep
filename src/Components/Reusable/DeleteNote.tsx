00cc14e8df3b184d0075aad418fb32e17e1fa991
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 16:42:28 +0530

    update : change icon

--- src/Components/Reusable/DeleteNote.tsx
+++ src/Components/Reusable/DeleteNote.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MdiTrashCanOutline } from '../../Svgs/Svgs'
+import { NotoV1Wastebasket } from '../Svgs/Svg'
 
 export const DeleteNote: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
         <div className="tooltip" onClick={onClick}>
-        <MdiTrashCanOutline />
+        <NotoV1Wastebasket />
         <span className="tooltiptext">Delete Note</span>
     </div>
     )
