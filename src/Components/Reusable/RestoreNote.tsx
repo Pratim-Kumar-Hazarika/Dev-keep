9db89a0b68685a6f02d8248e3a244e63d1beeb1a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 16:43:13 +0530

    update : change restore note icon

--- src/Components/Reusable/RestoreNote.tsx
+++ src/Components/Reusable/RestoreNote.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MdiDeleteRestore } from '../../Svgs/Svgs'
+import { TwemojiRecyclingSymbol } from '../Svgs/Svg'
 
 export const RestoreNote: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
     <div className="tooltip" onClick={onClick}>
-        <MdiDeleteRestore />
+        <TwemojiRecyclingSymbol />
             <span className="tooltiptext">Restore</span>
     </div>
     )
