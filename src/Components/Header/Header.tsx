13376f43aee4bac589eaaf8076dd91f4a870d047
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-08 16:40:18 +0530

    change : app logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import {BxBxSearchAlt2,AkarIconsCross,LogosGoogleKeep,IcOutlineWbSunny,CarbonUserAvatar} from "../../Svgs/Svgs"
 import "./Header.css";
 import {Link} from "react-router-dom"
+import { EmojioneV1NotePage } from "../Svgs/Svg";
 
 function Header() {
     return (
         <div className="header">
             <div className="logo_text">
-            <LogosGoogleKeep/> 
+            <EmojioneV1NotePage/> 
             <span className="keep">Keep</span>
             </div>
-            
             <div className="search-container">
                 <div className="flex">
                 <div className="search-container-searchbar-text">
@@ -23,9 +23,6 @@ function Header() {
             </div>
 
             <div className="flex">
-                <div className="mt-1 ml">
-                    <IcOutlineWbSunny/>
-                </div>
                 <Link to="/profile">
                 <div className="mt-1 ml">
                     <CarbonUserAvatar/>
