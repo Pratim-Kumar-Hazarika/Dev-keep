5d71fb1b3a66391ba05ebc5c688fc2488b163bd1
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-09 00:11:15 +0530

    update : add props

--- src/Components/Reusable/AddImage.tsx
+++ src/Components/Reusable/AddImage.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { BxBxImageAlt } from '../../Svgs/Svgs'
-import { FxemojiFramewithpicture } from '../Svgs/Svg'
+import React, { useState } from 'react'
+import { useGoogleKeep } from '../../Context/GoogleKeepProvider';
+import {BxBxImageAlt} from '../../Svgs/Svgs'
+import {FxemojiFramewithpicture} from '../Svgs/Svg'
 
-function AddImage() {
-return (
-<div className="tooltip">
-    <FxemojiFramewithpicture />
-    <span className="tooltiptext">Add image</span>
-</div>
-)
+export type PropsTypes = {
+  from :string
+  noteId?:number
+}
+function AddImage({from,noteId}:PropsTypes) {
+  console.log({from})
+  console.log({noteId})
+  console.log("clicked")
+   const {previewImage, setPreviewImageSource} = useGoogleKeep()
+    function handeInputChange(e:any) {
+        const file = e.target.files[0];
+        previewFile(file);
+      }
+      function previewFile(file:any) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+          setPreviewImageSource(reader.result);
+        };
+      }
+    return (
+        <div className="tooltip">
+            <label style={{cursor:"pointer"}}>
+                <FxemojiFramewithpicture/>
+                <input
+                 onChange={handeInputChange}
+                    type="file"
+                    style={{
+                    display: "none",
+
+                }}/>
+            </label>
+            <span className="tooltiptext">Add image</span>
+        </div>
+    )
 }
 
 export default AddImage
