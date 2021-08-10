3c4e2cf11bc3c9a5df1796981dcc743c8fc811ec
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-10 23:25:33 +0530

    update : set textarea height

--- src/Context/utils/PinClickHandler.ts
+++ src/Context/utils/PinClickHandler.ts
@@ -1,11 +1,12 @@
 import { AddNoteHandler } from './addNoteHandler';
 
-export function pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource}:AddNoteHandler){
+export function pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource,textRef}:AddNoteHandler){
     if(title || description !==''){
-    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,image:previewImage}})
+    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,images:[previewImage]}})
     setTitle("");
     setDescription('')
     setBgColor("")
     setPreviewImageSource("")
+    textRef.current.style.height = "25px";
     }
 }
--- src/Context/utils/archiveNoteClickHandler.ts
+++ src/Context/utils/archiveNoteClickHandler.ts
@@ -2,12 +2,13 @@ import { AddNoteHandler } from './addNoteHandler';
 
 
 
-export function archiveClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource}:AddNoteHandler){
+export function archiveClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource,textRef}:AddNoteHandler){
     if(title || description !== ''){
-    dispatch({type:"ARCHIVE_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,image:previewImage}})
+    dispatch({type:"ARCHIVE_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,images:[previewImage]}})
     setTitle("");
     setDescription('')
     setBgColor("")
     setPreviewImageSource("")
+    textRef.current.style.height = "25px";
     }
 }
