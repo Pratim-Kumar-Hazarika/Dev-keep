7588de841f2fe97c0f0e93e4d2382db93a916d0f
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-10 23:26:09 +0530

    add: types

--- src/Context/utils/addNoteHandler.ts
+++ src/Context/utils/addNoteHandler.ts
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from 'react';
 import { ACTION } from '../reducer/actions';
+import { Image } from '../types';
 
 export type AddNoteHandler = {
     e:any
@@ -11,19 +12,21 @@ export type AddNoteHandler = {
    setDescription:Dispatch<SetStateAction<string>>;
    setBgColor:Dispatch<SetStateAction<string>>;
    setLabel:Dispatch<SetStateAction<string>>;
-   previewImage:any;
-   setPreviewImageSource:any
+   previewImage:Image ;
+   setPreviewImageSource:any;
+   textRef:React.MutableRefObject<any>
 
 }
 
-export function addNoteHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource}:AddNoteHandler){
+export function addNoteHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource,textRef}:AddNoteHandler){
         e.preventDefault()
         if(title || description !== ''){
-        dispatch({type:"ADD_NOTE",payload:{id:Math.random() ,title:title,description:description,label:[],color:bgColor,image:previewImage}})
+        dispatch({type:"ADD_NOTE",payload:{id:Math.random() ,title:title,description:description,label:[],color:bgColor,images:[previewImage]}})
         setTitle("");
         setDescription('')
         setBgColor("")
         setLabel("")
         setPreviewImageSource("")
+        textRef.current.style.height = "25px";
         }
     }
