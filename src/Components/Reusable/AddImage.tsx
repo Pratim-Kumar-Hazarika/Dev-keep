806a3fd69743b7851a070541b1399d5465291205
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-10 22:04:15 +0530

    update : add dispatch for image

--- src/Components/Reusable/AddImage.tsx
+++ src/Components/Reusable/AddImage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGoogleKeep } from '../../Context/GoogleKeepProvider';
 import {BxBxImageAlt} from '../../Svgs/Svgs'
 import {FxemojiFramewithpicture} from '../Svgs/Svg'
@@ -8,10 +8,8 @@ export type PropsTypes = {
   noteId?:number
 }
 function AddImage({from,noteId}:PropsTypes) {
-  console.log({from})
-  console.log({noteId})
-  console.log("clicked")
-   const {previewImage, setPreviewImageSource} = useGoogleKeep()
+   const {previewImage, setPreviewImageSource,dispatch} = useGoogleKeep()
+   const [imageSrc,setImgSrc] = useState<any>()
     function handeInputChange(e:any) {
         const file = e.target.files[0];
         previewFile(file);
@@ -20,10 +18,31 @@ function AddImage({from,noteId}:PropsTypes) {
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = () => {
+          if(from === "input"){
           setPreviewImageSource(reader.result);
-        };
+          }
+          setImgSrc(reader.result)
+        };     
+  
       }
+      useEffect(()=>{
+       ( function (){
+         if(imageSrc!=="" && imageSrc !== undefined){
+          if(from==="card" ){
+            dispatch({type:"ADD_IMAGE_TO_NOTE",payload:{noteId:noteId,images:{image:imageSrc}}})
+          }else if(from==="pinnedCard"){
+            dispatch({type:"ADD_IMAGE_TO_PINNED_NOTE",payload:{noteId:noteId,images:{image:imageSrc}}})
+          }else{
+            dispatch({type:"ADD_IMAGE_TO_ARCHIVED_NOTE",payload:{noteId:noteId,images:{image:imageSrc}}})
+          }
+          setPreviewImageSource("")
+          setImgSrc("")
+         }    
+        })()
+      },[imageSrc])
+     
     return (
+      <>
         <div className="tooltip">
             <label style={{cursor:"pointer"}}>
                 <FxemojiFramewithpicture/>
@@ -37,6 +56,7 @@ function AddImage({from,noteId}:PropsTypes) {
             </label>
             <span className="tooltiptext">Add image</span>
         </div>
+        </>
     )
 }
 
