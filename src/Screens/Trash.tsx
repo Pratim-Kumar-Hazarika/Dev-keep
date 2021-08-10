7d5cb8f5762cfbd78a62a60e6b50e7112f5df7ba
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-10 22:04:39 +0530

    update : fix label and image

--- src/Screens/Trash.tsx
+++ src/Screens/Trash.tsx
@@ -2,7 +2,9 @@ import EditLabel from '../Components/EditLabel/EditLabel'
 import Header from '../Components/Header/Header'
 import LeftNav from '../Components/LeftNavBar/LeftNav'
 import { DeleteForever } from '../Components/Reusable/DeleteForever'
+import { DisplayImage } from '../Components/Reusable/DisplayImage'
 import { RestoreNote } from '../Components/Reusable/RestoreNote'
+import { SmallImages } from '../Components/Reusable/SmallImages'
 import { useGoogleKeep } from '../Context/GoogleKeepProvider'
 import { Notes } from '../Context/types'
 
@@ -15,14 +17,34 @@ return (
     <div style={{display:"flex",opacity:keepOpacity? "0.4":"1"}}>
         <LeftNav />
         <div className="trash_cards">
-            { state?.trash?.map(({title,description,label,id,color,image}:Notes)=>{
-            return <><div key={id} className="card_div" style={{backgroundColor:color}}>
-                 <div className="uplod_img_div">
-                <img className="uplod_img" src={image}/>
-            </div>
+            { state?.trash?.map(({title,description,label,id,color,images}:Notes)=>{
+            return <>
+             <div className="card_div" style={{backgroundColor:color}}>
+              {
+                    images?.slice(1,2).map((image)=>(
+                        <>
+                        <DisplayImage image={image.image} onClick={()=>dispatch({type:"DELETE_IMAGE",payload:{noteId:id,imageId:image.image}})}/>
+                        </>
+                    ))
+                }
+               <div className="img_flex">
+               {
+                images?.slice(2).map((image)=>(
+                            <SmallImages image={image.image}/>
+                        ))
+                }
+                </div>
+                 
                 <span> {title}</span>
                 <p>{description}</p>
-                <div className="label">"lolo"</div>
+                <div className="label__flex">
+             
+             {
+                 label.map((label)=>(
+                     <div className="label">{label.labelName}</div>
+                 ))
+             }
+           </div>
                 <div className="trash_card_icons">
                     <RestoreNote onClick={()=>dispatch({type:"RESTORE_NOTE",payload:{id}})}/>
                     <DeleteForever onClick={()=> dispatch({type:"DELETE_FOREVER",payload:{id}})}/>
