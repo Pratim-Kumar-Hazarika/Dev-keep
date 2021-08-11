b0b09cd3d9d6f7e36123e7950ee2f57de8d7dc82
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-11 22:46:51 +0530

    preetify

--- src/Context/utils/PinClickHandler.ts
+++ src/Context/utils/PinClickHandler.ts
@@ -1,12 +1,53 @@
-import { AddNoteHandler } from './addNoteHandler';
+import {AddNoteHandler} from './addNoteHandler';
 
-export function pinClickHandler({e,title,description,bgColor,dispatch,setTitle,setDescription,setBgColor,setLabel,previewImage,setPreviewImageSource,textRef}:AddNoteHandler){
-    if(title || description !==''){
-    dispatch({type:"PIN_NOTE_DIRECTLY",payload:{id:Math.random(),title:title,description:description,label:[],color:bgColor,images:[previewImage]}})
-    setTitle("");
-    setDescription('')
-    setBgColor("")
-    setPreviewImageSource("")
-    textRef.current.style.height = "25px";
+export function pinClickHandler({
+    e,
+    title,
+    description,
+    bgColor,
+    dispatch,
+    setTitle,
+    setDescription,
+    setBgColor,
+    setLabel,
+    previewImage,
+    setPreviewImageSource,
+    textRef
+} : AddNoteHandler) {
+    if (title || description !== '') {
+        if (previewImage) {
+            dispatch({
+                type: "PIN_NOTE_DIRECTLY",
+                payload: {
+                    id: Math.random(),
+                    title: title,
+                    description: description,
+                    label: [],
+                    color: bgColor,
+                    images: [
+                        {
+                            image: previewImage
+                        }
+                    ]
+                }
+            })
+        } else {
+            dispatch({
+                type: "PIN_NOTE_DIRECTLY",
+                payload: {
+                    id: Math.random(),
+                    title: title,
+                    description: description,
+                    label: [],
+                    color: bgColor,
+                    images: []
+                }
+            })
+        }
+        setTitle("");
+        setDescription('')
+        setBgColor("")
+        setPreviewImageSource("")
+        textRef.current.style.height = "25px";
     }
 }
