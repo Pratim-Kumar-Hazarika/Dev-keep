950348be06f9f06a4cb97b61a93becd29a0a0a41
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-11 22:49:38 +0530

    refactor

--- src/Components/ShowEditModel/EditModel.tsx
+++ src/Components/ShowEditModel/EditModel.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useLocation, useParams } from 'react-router'
 import { useGoogleKeep } from '../../Context/GoogleKeepProvider'
-import { Image, NoteLabelTypes } from '../../Context/types'
+import { Image, NoteLabelTypes, Notes, ReducerInitialState } from '../../Context/types'
+import { filterNoteToBeEdited } from '../../Context/utils/filterNoteToBeEdited'
+import Card from '../Card/Card'
 import { EditNote } from '../EditNote/EditNote'
+import Header from '../Header/Header'
+import Input from '../Input/Input'
+import LeftNav from '../LeftNavBar/LeftNav'
+import PinnedCard from '../PinnedCards/PinnedCard'
 
 export type EditNoteProps = {
     noteTitle:string;
@@ -13,11 +20,45 @@ export type EditNoteProps = {
 }
 
 
-export const EditModel: React.FC<EditNoteProps> = ({noteTitle,noteDescription,noteColor,noteImages,noteId,noteLables}) => {
-    const {showEditNoteModel} = useGoogleKeep()
+export const EditModel: React.FC<{}> = ({}) => {
+    const {showEditNoteModel,state,keepOpacity} = useGoogleKeep()
+    const {noteId} = useParams()
+    const {from} = useParams()
+    const { filterNote, filterPinnedNote, filterArchiveNote } = filterNoteToBeEdited(state, noteId)
+    const {title,id,description,label,color,images} = filterNote[0] || filterPinnedNote[0] || filterArchiveNote[0] || {}
+    
+    const filterNonEditedPinnedNotes = state.pinnedNotes.filter((note)=>note.id !== Number(noteId))
+    const filterNonEditedNotes = state.notes.filter((note)=>note.id !== Number(noteId))
+
     return (
+        <>
+           <Header />
         <div className="edit_model"  >
-           {/* <EditNote noteTitle={noteTitle} noteDescription={noteDescription} noteColor={noteColor} noteImages={noteImages} noteId={noteId} noteLables={noteLables}/> */}
+           <EditNote from={from} title={title} description={description} color={color} image={images} id={id} label={label}/>
         </div>
+        <div className="keep" style={{opacity:keepOpacity? "0.4":"1"}}>
+        <LeftNav />
+        <div className="main">
+            <Input />
+            <h5>{state?.pinnedNotes?.length >0 && "PINNED"}</h5>
+            <div className="flex-wrap" >
+                {
+                filterNonEditedPinnedNotes?.map(({title,description,label,id,color,images}:Notes)=>{
+                return <PinnedCard from={"pinnedCard"} image={images} key={id} id={id} title={title} description={description} color={color} label={label}/> })
+                }
+            </div>
+            <br />
+            <h5>{state?.notes?.length >0 && "OTHERS"}</h5>
+            <div className="flex-wrap">
+                {
+               filterNonEditedNotes?.map(({title,description,id,color,label,images}:Notes)=>{
+                return<> <Card from={"card"} image={images} key={id} id={id} title={title} description={description} color={color} label={label}/> 
+                </>})
+                }
+            </div>
+        </div>
+    </div>
+        </>
     )
 }
+
