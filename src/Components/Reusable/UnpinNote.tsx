e7ac46697add24e67eb8c33377ece9f7b3ee5cc7
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-11 22:50:13 +0530

    fix : pin icon size

--- src/Components/Reusable/UnpinNote.tsx
+++ src/Components/Reusable/UnpinNote.tsx
@@ -4,7 +4,7 @@ import { FxemojiPushpin, MdiPin } from '../../Svgs/Svgs'
 export const UnpinNote: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
     <div className="tooltip" onClick={onClick}>
-              <FxemojiPushpin/>
+              <FxemojiPushpin style={{fontSize:"0.9rem"}}/>
             <span className="tooltiptext">Unpin Note</span>
     </div>
     )
