524afa8bad007b5e677e1745b5188da1aaca0879
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-11 22:51:04 +0530

    no change

--- src/Components/Reusable/MediumImages.tsx
+++ src/Components/Reusable/MediumImages.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { NotoV1CrossMark } from '../Svgs/Svg'
 
 export const MediumImages: React.FC<{image:string}> = ({image}) => {
-    return (
-            <img className="flex_medium_img" src={image}/> 
+    return (<>
+            <img className="flex_medium_img" src={image}/>
+          
+            </>
     )
 }
