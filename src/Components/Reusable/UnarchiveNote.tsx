38158891f934439427321d2aa21cc3ec4b2fde45
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-11 22:50:24 +0530

    fix : unarchive icon

--- src/Components/Reusable/UnarchiveNote.tsx
+++ src/Components/Reusable/UnarchiveNote.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BxBxArchiveOut } from '../../Svgs/Svgs'
+import { FxemojiOutbox } from '../Svgs/Svg'
 
 export const UnarchiveNote: React.FC<{onClick?:React.MouseEventHandler<HTMLElement>}> = ({onClick}) => {
     return (
         <div className="tooltip" onClick={onClick}>
-        <BxBxArchiveOut />
+        <FxemojiOutbox />
             <span className="tooltiptext">Unarchive</span>
     </div>
     )
