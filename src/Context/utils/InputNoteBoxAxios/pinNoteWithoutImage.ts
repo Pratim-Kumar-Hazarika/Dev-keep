5a7bc4317c735ea59b2407dcb1d4a2ceb7223a08
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:12:06 +0530

    Create pinNoteWithoutImage.ts

---
+++ src/Context/utils/InputNoteBoxAxios/pinNoteWithoutImage.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export async function pinNoteFromInputWithoutImage(previewImage: string, title: string, description: string, bgColor: string, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/pinnedNote/onlytext", {
+            image: [],
+            title: title,
+            description: description,
+            label: [],
+            color: bgColor
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status === 200) {
+            console.log("note added  sucessfully from input to pinned");
+        }
+    } catch (error) {
+        console.log("error occured while adding the note  to pinned");
+    }
+}
