a83866880f660dcf6f1692a1387239d32bbc5831
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:21:34 +0530

    Create addLabelToPinnedNote.ts

---
+++ src/Context/utils/AddLabelToParticularNoteAxios/addLabelToPinnedNote.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export async function addLabelToPinnedNote(noteId: any, labelName: string, id: number, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/pinnedNote/label", {
+            noteId: noteId,
+            labelName: labelName,
+            _id: id
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status == 200) {
+            console.log("label added sucessfully to pinned note");
+        }
+    } catch {
+        console.log("error occured while adding the label to pinned note");
+    }
+}
