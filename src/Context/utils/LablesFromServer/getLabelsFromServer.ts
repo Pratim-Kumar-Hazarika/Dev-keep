53784d4de34ff999ae7cbabfa1ad77c8ca735994
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:12:11 +0530

    Create getLabelsFromServer.ts

---
+++ src/Context/utils/LablesFromServer/getLabelsFromServer.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../../reducer/actions";
+
+export type GetUserNotesFromServer = {
+    dispatch :Dispatch<ACTION>;
+    token:string;
+}
+export function getUserLabelsFromServer({dispatch,token}:GetUserNotesFromServer): void {
+    (async function () {
+        try {
+            const response = await axios.get(`http://localhost:8080/user/labels`,{
+                headers:{
+                    authorization:token
+                }
+            });
+          const getLabels = response.data.getLables.labels.map((label:any)=>(
+              {
+                labelName:label.labelName,
+                id:label._id 
+              }
+          ))
+            if (response.status === 200) {
+                dispatch({ type: "GET_LABLES_FROM_SERVER", payload: { allLabels:getLabels} });            
+            }
+            return response.data.videos;
+        } catch (error) {
+           return error;
+        }
+    })();
+}
