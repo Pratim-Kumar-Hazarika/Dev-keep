ad59dcc3f5bc972cb05ceef2b8b3c5eb72624b9c
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:21:37 +0530

    Create deleteLabelFromArchiveNote.ts

---
+++ src/Context/utils/AddLabelToParticularNoteAxios/deleteLabelFromArchiveNote.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export async function deleteLabelFromArchivedNote(noteId: any, id: number, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/archiveNote/label/delete", {
+            noteId: noteId,
+            labelId: id
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status == 200) {
+            console.log("label deleted sucessfully to from archived note");
+        }
+    } catch {
+        console.log("error occured while deleting the label from archived note");
+    }
+}
