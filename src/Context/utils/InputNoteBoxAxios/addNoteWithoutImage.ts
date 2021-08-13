73af72e60d3c740665c5da52ca304ca1f729de27
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:11:41 +0530

    Create addNoteWithoutImage.ts

---
+++ src/Context/utils/InputNoteBoxAxios/addNoteWithoutImage.ts
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction } from 'react';
+import axios from "axios";
+
+export async function addNoteWithoutImage(title: string, description: string, bgColor: string, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/notes/onlytext", {
+            image: [],
+            title: title,
+            description: description,
+            label: [],
+            color: bgColor,
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status === 200) {
+            console.log("note added  sucessfully from input");
+        }
+    } catch (error) {
+        console.log("error occured while adding the note");
+    }
+}
