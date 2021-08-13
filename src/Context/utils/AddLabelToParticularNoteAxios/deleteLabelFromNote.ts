a851a399d6a093da7ddeaac38784d7d46065244a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:21:40 +0530

    Create deleteLabelFromNote.ts

---
+++ src/Context/utils/AddLabelToParticularNoteAxios/deleteLabelFromNote.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export async function deleteLabelFromNote(noteId: any, id: number, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/notes/label/delete", {
+            noteId: noteId,
+            labelId: id
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status == 200) {
+            console.log("label deleted sucessfully to particular note");
+        }
+    } catch {
+        console.log("error occured while adding the label");
+    }
+}
