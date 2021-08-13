7ee27e9708544b9013e2bad431607fc86c91fbb0
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:48:28 +0530

    Create changeColorFromEditModel.ts

---
+++ src/Context/utils/EditNote/changeColorFromEditModel.ts
@@ -0,0 +1,19 @@
+import { Dispatch } from 'react';
+import { ACTION } from '../../reducer/actions';
+import { changeArchiveNotesBg } from '../ArchiveNotesAxios/changebgArchivedNote';
+import { changeOtherNotesBg } from '../OtherNotesAxios/changeBgColorFromOthers';
+import { changePinnedNotesBg } from '../PinnedNotesAxios/changeBgPinnedNote';
+
+export async function changeColorFromEditModel(color:any,dispatch:Dispatch<ACTION>,id:any,from:string,token:string){
+    if(from ==="card"){
+        dispatch({type:"CHANGE_OTHER_NOTES_BG",payload:{colorName:color,id:id}})
+         await changeOtherNotesBg({color,id,dispatch,token})
+    }else if(from =="pinnedCard"){
+        dispatch({type:"CHANGE_PINNED_NOTES_BG",payload:{colorName:color,id:id}})
+       await changePinnedNotesBg({color,id,dispatch,token})
+    }else if(from ==="archive"){
+        dispatch({type:"CHANGE_ARCHIVED_NOTES_BG",payload:{colorName:color,id:id}})
+       await changeArchiveNotesBg({color,id,dispatch,token})
+    }
+    
+}
