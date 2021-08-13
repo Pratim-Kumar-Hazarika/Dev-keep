32ab8333f6df37c9d188eb2bb78056ef72c158b4
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:10:08 +0530

    Create pinArchivedNote.ts

---
+++ src/Context/utils/ArchiveNotesAxios/pinArchivedNote.ts
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { OthersNote } from "../PinnedNotesAxios/unpinNote"
+
+export async function pinArchivedNotes({dispatch,id,token}:OthersNote){
+    dispatch({type: "PIN_ARCHIVED_NOTE", payload: {id}})
+    try {
+        const response = await axios.post("http://localhost:8080/user/archiveNote/pinned",{
+            noteId:id
+        },{
+            headers: {
+                authorization: token
+            }
+        })
+        if(response.status === 200){
+            console.log("archive note pinned successfully")
+        }
+    } catch (error) {
+        console.log("error occured while pinning archived note")
+    }
+   }
