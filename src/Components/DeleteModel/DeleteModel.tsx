c351076ee3739ff2861c5684bb9e24131542e97a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:04:29 +0530

    update : add db + server

--- src/Components/DeleteModel/DeleteModel.tsx
+++ src/Components/DeleteModel/DeleteModel.tsx
@@ -1,10 +1,29 @@
+import axios from "axios"
+import { useAuth } from "../../Context/AuthProvider"
 import { useGoogleKeep } from "../../Context/GoogleKeepProvider"
 
 export const DeleteModel: React.FC<{id:number,labelName:string}> = ({id,labelName}) => {
-    const {showDeleteModel,setShowDeleteModel,dispatch} = useGoogleKeep()
-    function deleteLabelHandler(){
+    const {showDeleteModel,setShowDeleteModel,setShowLabelModel,dispatch,setKeepOpacity} = useGoogleKeep()
+    const {token} = useAuth()
+  async function deleteLabelHandler(){
         dispatch({type:"DELETE_LABELS",payload:{labelName:labelName,id:id}})
         setShowDeleteModel("hidden")
+        setShowLabelModel("hidden")
+        setKeepOpacity(false)
+        try {
+            const response = await axios.post("http://localhost:8080/user/labels/delete",{
+                labelId:id
+            },{
+                headers: {
+                    authorization: token
+                }
+            })
+            if(response.status==200){
+                console.log("label deleted sucessfully")
+            }
+        } catch (error) {
+            console.log("error while deleting the label")
+        }
     }
     return (
         <div className="delete_div" style={{ visibility:showDeleteModel}}>
