127f8073690166c011757a03e32c099a3c335258
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:11:52 +0530

    Create archiveNoteWithoutImage.ts

---
+++ src/Context/utils/InputNoteBoxAxios/archiveNoteWithoutImage.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export async function archiveNoteWithoutImage(previewImage: string, title: string, description: string, bgColor: string, token: string) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/archiveNote/onlytext", {
+            image: [],
+            title: title,
+            description: description,
+            label: [],
+            color: bgColor,
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status === 200) {
+            console.log("note added  sucessfully from input to archive");
+        }
+    } catch (error) {
+        console.log("error occured while adding the note from archive");
+    }
+}
