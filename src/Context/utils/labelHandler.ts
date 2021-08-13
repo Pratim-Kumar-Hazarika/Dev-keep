de730041aa2b367c53b66c337959a64b01f42003
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:21:59 +0530

    Update labelHandler.ts

--- src/Context/utils/labelHandler.ts
+++ src/Context/utils/labelHandler.ts
@@ -1,29 +1,53 @@
+import axios from 'axios';
 import { Dispatch } from 'react';
 import { ACTION } from '../reducer/actions';
 import { ReducerInitialState } from './../types';
+import { addLabelToArchivedNote } from './AddLabelToParticularNoteAxios/addLabelToArchiveNote';
+import { addLabelToNote } from './AddLabelToParticularNoteAxios/addLabelToNote';
+import { addLabelToPinnedNote } from './AddLabelToParticularNoteAxios/addLabelToPinnedNote';
+import { deleteLabelFromArchivedNote } from './AddLabelToParticularNoteAxios/deleteLabelFromArchiveNote';
+import { deleteLabelFromNote } from './AddLabelToParticularNoteAxios/deleteLabelFromNote';
+import { deleteLabelFromPinnedNote } from './AddLabelToParticularNoteAxios/deleteLabelFromPinnedNote';
 import { checkLabelInNotesHandler } from "./checkBoxIdLabelHandler";
 import { dispatchHandler } from './dispatchLabelHandler';
 
 export type NoteLabelHandlers = {
     state:ReducerInitialState;
-    noteId:number;
+    noteId:any;
     id:number;
     dispatch:Dispatch<ACTION>;
     labelName:string;
+    token:string
 }
 
-export function notesLabelHandler({state,noteId,id,dispatch,labelName}:NoteLabelHandlers) {
+export async function notesLabelHandler({state,noteId,id,dispatch,labelName,token}:NoteLabelHandlers) {
     const getNote = state.notes.filter((note) => note.id === noteId);
     const checkForLabel = checkLabelInNotesHandler(getNote, id);
     dispatchHandler(checkForLabel, dispatch, id, noteId, labelName);
+    if(checkForLabel){
+        await deleteLabelFromNote(noteId, id, token);
+    }else {
+        await addLabelToNote(noteId, labelName, id, token);
+    }
 }
-export function pinnedNotesLabelHandler({state,noteId,id,dispatch,labelName}:NoteLabelHandlers) {
+
+export async function pinnedNotesLabelHandler({state,noteId,id,dispatch,labelName,token}:NoteLabelHandlers) {
     const getNote = state.pinnedNotes.filter((note) => note.id === noteId);
     const checkForLabel = checkLabelInNotesHandler(getNote, id);
     dispatchHandler(checkForLabel, dispatch, id, noteId, labelName);
+    if(checkForLabel){
+        await deleteLabelFromPinnedNote(noteId, id, token);
+    }else {
+        await addLabelToPinnedNote(noteId, labelName, id, token);
+    }
 }
-export function archiveLabelHandler({state,noteId,id,dispatch,labelName}:NoteLabelHandlers) {
+export async function archiveLabelHandler({state,noteId,id,dispatch,labelName,token}:NoteLabelHandlers) {
     const getNote = state.archive.filter((note) => note.id === noteId);
     const checkForLabel = checkLabelInNotesHandler(getNote, id);
     dispatchHandler(checkForLabel, dispatch, id, noteId, labelName);
+    if(checkForLabel){
+        await deleteLabelFromArchivedNote(noteId, id, token);
+    }else {
+        await addLabelToArchivedNote(noteId, labelName, id, token);
+    }
 }
