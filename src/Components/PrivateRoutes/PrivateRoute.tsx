8e0671c96b7ac3a0819b03b8041bfa8bc5c3c613
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:06:12 +0530

    create : add private route

---
+++ src/Components/PrivateRoutes/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Navigate, Route } from 'react-router'
+import { useAuth } from '../../Context/AuthProvider'
+
+
+export const PrivateRoute: React.FC<{path:any,element:any}> = ({path,...props}) => {
+    const {token} = useAuth()
+    return token ? <Route {...props} path={path}/> :<Navigate  replace to="/login"/>
+}
