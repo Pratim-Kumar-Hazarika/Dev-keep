7720570182ad884ad8df6780054789685c21d31b
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:11:12 +0530

    Update filterNoteToBeEdited.ts

--- src/Context/utils/filterNoteToBeEdited.ts
+++ src/Context/utils/filterNoteToBeEdited.ts
@@ -1,7 +1,8 @@
 import { ReducerInitialState } from './../types';
-export function filterNoteToBeEdited(state: ReducerInitialState, noteId: string) {
-    const filterNote = state.notes.filter((note) => note.id === Number(noteId))
-    const filterPinnedNote = state.pinnedNotes.filter((note) => note.id === Number(noteId))
-    const filterArchiveNote = state.archive.filter((note) => note.id === Number(noteId))
+export function filterNoteToBeEdited(state: ReducerInitialState, noteId: string | any) {
+    const filterNote = state.notes.filter((note) => note.id === noteId)
+    console.log("filter note is",filterNote)
+    const filterPinnedNote = state.pinnedNotes.filter((note) => note.id === noteId)
+    const filterArchiveNote = state.archive.filter((note) => note.id === noteId)
     return { filterNote, filterPinnedNote, filterArchiveNote }
 }
