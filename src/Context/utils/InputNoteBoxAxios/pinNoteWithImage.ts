2e264a71cb9ea8ebe3bae45f48eb35b2e4f20172
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:11:57 +0530

    Create pinNoteWithImage.ts

---
+++ src/Context/utils/InputNoteBoxAxios/pinNoteWithImage.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { AddNoteHandler } from "../addNoteHandler";
+
+export async function pinNoteFromInputWithImage(previewImage: string, title: string, description: string, bgColor: string, token: string,) {
+    try {
+        const response = await axios.post("http://localhost:8080/user/pinnedNote", {
+            imageUrl: previewImage,
+            title: title,
+            description: description,
+            label: [],
+            color: bgColor
+        }, {
+            headers: {
+                authorization: token
+            }
+        });
+        if (response.status === 200) {
+            console.log("note added  sucessfully from input to pinned");
+        }
+    } catch (error) {
+        console.log("error occured while adding the note  to pinned");
+    }
+}
