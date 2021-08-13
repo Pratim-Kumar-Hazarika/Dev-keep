07534dd7014e163b909a1ae5135937e1969841ee
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 23:33:23 +0530

    Create createLabel.ts

---
+++ src/Context/utils/CreateDeleteEditLabelAxios/createLabel.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { Dispatch, SetStateAction } from 'react';
+import { ACTION } from '../../reducer/actions';
+
+export type AddLabelClickHandler = {
+    e:any;
+    btntext:string;
+    dispatch:Dispatch<ACTION>;
+    setBtnText:Dispatch<SetStateAction<string>>
+    token:string
+}
+export async function addLabelClickHandler({e,btntext,dispatch,setBtnText,token}:AddLabelClickHandler){
+    e.preventDefault()
+    if(btntext!==""){
+        dispatch({type:"ADD_LABEL",payload:{labelName:btntext,id:Math.random()}})
+        setBtnText("");
+        try{
+            const response = await axios.post("http://localhost:8080/user/labels",{
+                labelName:btntext
+            },{
+                headers: {
+                    authorization: token
+                }
+            })
+            if(response.status==200){
+                console.log("label added sucessfully")
+            }
+        }catch{
+            console.log("error occured while adding the label")
+        }
+    }
+}
