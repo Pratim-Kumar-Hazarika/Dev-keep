9c7a9b0f9a1cd57c720bbd7cd07377466931dbb0
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:02:42 +0530

    update : make routes private

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,23 @@ import { useGoogleKeep } from './Context/GoogleKeepProvider';
 import { EditNote } from './Components/EditNote/EditNote';
 import { EditModel } from './Components/ShowEditModel/EditModel';
 import { EditArchiveNotes } from './Components/ShowEditModel/EditArchiveNotes';
+import { PrivateRoute } from './Components/PrivateRoutes/PrivateRoute';
 
 function App() {
+  const {state} = useGoogleKeep()
+  console.log({state})
   return (
     <div className="App">
         <Routes>
-          <Route path ="/home" element ={<GoogleKeep/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path ="/trash" element={<Trash/>}/>
-          <Route path = "/archive" element = {<Archive/>}/>
-          <Route path = "/login" element={<Login/>}/>
+         <Route path = "/login" element={<Login/>}/>
           <Route path = "/signup" element={<Signup/>}/>
-          <Route path = "/label/:labelName" element={<Label/>}/>
-          <Route path = "/home/:from/:noteId" element ={<EditModel/>}/>
-          <Route path = "/note/:from/:noteId" element ={<EditArchiveNotes/>}/>
+          <PrivateRoute path ="/home" element ={<GoogleKeep/>}/>
+          <PrivateRoute path="/profile" element={<Profile/>}/>
+          <PrivateRoute path ="/trash" element={<Trash/>}/>
+          <PrivateRoute path = "/archive" element = {<Archive/>}/>
+          <PrivateRoute path = "/label/:labelName" element={<Label/>}/>
+          <PrivateRoute path = "/home/:from/:noteId" element ={<EditModel/>}/>
+          <PrivateRoute path = "/note/:from/:noteId" element ={<EditArchiveNotes/>}/>
         </Routes>
     </div>
   );
