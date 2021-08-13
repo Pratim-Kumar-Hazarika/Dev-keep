1e151b04fb5cf8fbc15a32552a308ee5f7ff5631
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:05:32 +0530

    update : add formik + validations + api calls

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,24 +1,66 @@
 import { Link } from 'react-router-dom'
 import "./Login.css"
 
+import { validateEmail, validatePassword } from '../../Context/utils/Validations';
+import { useAuth } from '../../Context/AuthProvider';
+import ClipLoader from "react-spinners/ClipLoader";
+import { Field, Form, Formik } from 'formik';
+interface MyFormValues {
+    password: string;
+    email: string;
+  }
+
+  
 export default function LoginForm() {
-    return (<> <div className="login_content">
+    const initialValues: MyFormValues = { password: "", email: "" };
+    const {email,setEmail,password,setPassword,loginUserWithCredentials,loading} = useAuth()
+
+  function userLogin(email:string,password:string){
+    loginUserWithCredentials(email,password)
+    
+  }
+ 
+    return (<> 
+    <div className="login_content">
         <div className="login_heading">
             <span
                 style={{
                 fontWeight: "bold"
             }}>Login</span>
         </div>
-        <div className="email_div">
+        <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ errors, touched, isValidating }) => (
+          <Form>
+            <div className="email_div">
             <div>Email</div>
-            <input className="email_input" type="text" name="" id=""/>
-        </div>
-        <div className="password_div">
+              <Field name="email" validate={(value:string)=>(validateEmail({value,setEmail}))} className="password-input"  />
+              {errors.email && touched.email && <div className="error_text">{errors.email}</div>}
+            </div>
+            
+            <div className="password_div">
             <div>Password</div>
-            <input className="password_input" type="text" name="" id=""/>
-        </div>
-        <div>
-            <button className="login_btn">Login</button>
+              <Field name="password" validate={(value:string)=>validatePassword({value,setPassword})} className="password-input" type="password"/>
+              {errors.password && touched.password && (
+                <div className="error_text">{errors.password}</div>
+              )}
+            </div>
+            <div>
+            <button style={{cursor:"pointer"}} type="submit" className="login_btn" onClick={()=>{userLogin(email,password)}}>
+              {!loading && "LOGIN"}
+              {loading && ""}
+              <div className="loader"> 
+               <ClipLoader
+                  color={"white"}
+                  loading={loading}
+                  size={25}
+                /></div>
+          
+            </button>
             <div className="dont_have_account_div">
                 <span>Don't have an account ?</span>
                 <Link to="/signup">
@@ -26,7 +68,10 @@ export default function LoginForm() {
                     Signup</span>
                 </Link>
             </div>
-        </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
     </>
     )
--- src/Components/Signup/SignupForm.tsx
+++ src/Components/Signup/SignupForm.tsx
@@ -1,38 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import "../Login/Login.css"
-
-export default function SignUpForm() {
-    return (<> <div className="login_content">
+import { Formik, Form, Field } from "formik";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useAuth } from '../../Context/AuthProvider';
+import { validateEmail, validatePassword, validateuserName } from '../../Context/utils/Validations';
+import { signUpHandler } from '../../Context/utils/signUpHandler';
+interface MyFormValues {
+    password: string;
+    email: string;
+    userName:string;
+  }
+export default function SignupForm() {
+    const initialValues: MyFormValues = { password: "", email: "",userName:"" };
+    const {email,setEmail,setPassword,password,name,setName,loading,setLoading} = useAuth();
+    const navigate = useNavigate()
+ 
+    return (<> 
+    <div className="login_content">
         <div className="login_heading">
             <span
                 style={{
                 fontWeight: "bold"
             }}>Signup</span>
         </div>
-        <div className="email_div">
+        <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ errors, touched, isValidating }) => (
+          <Form>
+              <div className="email_div">
             <div>Name</div>
-            <input className="email_input" type="text" name="" id=""/>
-        </div>
-        <div className="email_div">
+              <Field name="userName" validate={(value:string)=>validateuserName({value,setName})} className="password-input"/>
+              {errors.userName && touched.userName && <div className="error_text">{errors.userName}</div>}
+            </div>
+            <div className="email_div">
             <div>Email</div>
-            <input className="email_input" type="text" name="" id=""/>
-        </div>
-        <div className="password_div">
+              <Field name="email" validate={(value:string)=>validateEmail({value,setEmail})} className="password-input"/>
+              {errors.email && touched.email && <div className="error_text">{errors.email}</div>}
+            </div>
+            
+            <div className="password_div">
             <div>Password</div>
-            <input className="password_input" type="text" name="" id=""/>
-        </div>
-        <div>
-            <button className="login_btn">Signup</button>
+              <Field name="password" validate={(value:string)=>validatePassword({value,setPassword})} className="password-input" type="password"/>
+              {errors.password && touched.password && (
+                <div className="error_text">{errors.password}</div>
+              )}
+            </div>
+            <div>
+            <button type="submit" className="login_btn" onClick={()=>signUpHandler({name,email,password,navigate,setLoading})}>
+            {!loading && "SUBMIT"}
+              {loading && ""}
+            <div className="loader"> 
+               <ClipLoader
+                  color={"white"}
+                  loading={loading}
+                  size={25}
+                /></div>
+            </button>
             <div className="dont_have_account_div">
-                <span>Have an account ?</span>
+            <span>Have an account ?</span>
                 <Link to="/login">
                 <span className="signup_text">
                     Login</span>
                 </Link>
-               
             </div>
-        </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
     </>
     )
