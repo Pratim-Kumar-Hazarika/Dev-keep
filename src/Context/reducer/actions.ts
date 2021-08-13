5041b34d8dd932f99f37130a807d1504a17cd98f
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 01:13:00 +0530

    fix : send noteId also

--- src/Context/reducer/actions.ts
+++ src/Context/reducer/actions.ts
@@ -32,7 +32,7 @@ export type ACTION =
     | {type :"ADD_LABEL_TO_ALL_TYPE_OF_NOTES";payload:{noteId:number,label:{labelName:string,id:number}}}
     | {type :"EDIT_LABLES";payload:{labelName:string,id:number}}
     | {type :"DELETE_LABELS";payload:{labelName:string,id:number}}
-    | {type :"DELETE_LABELS_ONLY_FROM_NOTES";payload:{id:number}}
+    | {type :"DELETE_LABELS_ONLY_FROM_NOTES";payload:{noteId:number,id:number}}
     | {type :"ADD_IMAGE_TO_NOTE";payload:{noteId:number |undefined,images:{image:string}}}
     | {type :"ADD_IMAGE_TO_ARCHIVED_NOTE";payload:{noteId:number |undefined,images:{image:string}}}
     | {type :"ADD_IMAGE_TO_PINNED_NOTE";payload:{noteId:number |undefined,images:{image:string}}}
