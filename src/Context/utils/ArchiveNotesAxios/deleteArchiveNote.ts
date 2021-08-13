fbdb42f1e2761a5664af47149aa9d06dd3be4cae
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:10:01 +0530

    create delete util for archieve

---
+++ src/Context/utils/ArchiveNotesAxios/deleteArchiveNote.ts
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { OthersNote } from "../PinnedNotesAxios/unpinNote"
+
+
+export async function deleteArchivedNotes({dispatch,id,token}:OthersNote){
+    dispatch({type: "DELETE_ARCHIVED_NOTE", payload: { id}})
+    try {
+        const response = await axios.post("http://localhost:8080/user/archiveNote/delete",{
+            noteId:id
+        },{
+            headers: {
+                authorization: token
+            }
+        })
+        if(response.status === 200){
+            console.log("pinned note deleted successfully")
+        }
+    } catch (error) {
+        console.log("error occured while deleting  pinned note from others")
+    }
+}
