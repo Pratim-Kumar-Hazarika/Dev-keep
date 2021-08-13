6053fcbb531cea9de1c1cfa48c9da6b2ac1d0057
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 00:54:56 +0530

    Create addImageFromOthers.ts

---
+++ src/Context/utils/Image/ImageUplodAxios/addImageFromOthers.ts
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction } from 'react';
+import axios from "axios"
+import { ACTION } from '../../../reducer/actions';
+export type ImageUplod = {
+   noteId:any;
+   imageSrc:string;
+   token:string;
+   setImgSrc:Dispatch<SetStateAction<any>>
+}
+
+export async function addImageFromOthersNote({noteId,imageSrc,token,setImgSrc}:ImageUplod){
+    try {
+        const response = await axios.post("http://localhost:8080/user/notes/image",{
+            image:imageSrc,
+            noteId:noteId
+        },{
+            headers: {
+                authorization: token
+            }
+        })
+        if(response.status === 200){
+            console.log("image uplod sucessfully to pinned note")
+            setImgSrc('')
+        }
+    } catch (error) {
+        console.log("error occured while uploding the image to pinned note")
+    }
+}
