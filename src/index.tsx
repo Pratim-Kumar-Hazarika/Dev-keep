02d8fae3761583e66bdab3f34f5148c327ced321
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:13:46 +0530

    update : add authProvider + remove commented code

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom"
 import { GoogleKeepProvider } from './Context/GoogleKeepProvider';
+import { AuthProvider } from './Context/AuthProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <AuthProvider>
       <GoogleKeepProvider>
     <App />
     </GoogleKeepProvider>
+    </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
