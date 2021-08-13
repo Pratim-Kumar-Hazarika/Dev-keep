fd14f6e54e59b327223a424dbde5134618d48cfe
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 01:13:34 +0530

    fix : send noteId

--- src/Context/utils/dispatchLabelHandler.ts
+++ src/Context/utils/dispatchLabelHandler.ts
@@ -2,7 +2,7 @@ import { ACTION } from './../reducer/actions';
 import { NoteLabelTypes } from './../types';
 export function dispatchHandler(checkForLabel: NoteLabelTypes | undefined, dispatch: React.Dispatch<ACTION>, id: number, noteId: number, labelName: string) {
     if (checkForLabel) {
-        dispatch({ type: "DELETE_LABELS_ONLY_FROM_NOTES", payload: { id: id } });
+        dispatch({ type: "DELETE_LABELS_ONLY_FROM_NOTES", payload: { noteId:noteId,id: id } });
     } else {
         dispatch({ type: "ADD_LABEL_TO_ALL_TYPE_OF_NOTES", payload: { noteId: noteId, label: { labelName: labelName, id: id } } });
     }
