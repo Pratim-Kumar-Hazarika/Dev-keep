140fde024b169b702813c4c55fa8dcd2f322a40c
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:12:59 +0530

    Create unpinNote.ts

---
+++ src/Context/utils/PinnedNotesAxios/unpinNote.ts
@@ -0,0 +1,26 @@
+import { Dispatch } from 'react';
+import axios from "axios"
+import { ACTION } from '../../reducer/actions';
+
+export type OthersNote = {
+    dispatch :Dispatch<ACTION>;
+    id: number;
+    token:string
+}
+export async function unpinNote({dispatch,id,token}:OthersNote){
+    dispatch({type:"UNPIN_NOTE",payload:{id}})
+    try {
+        const response = await axios.post("http://localhost:8080/user/pinnedNote/note",{
+            noteId:id
+        },{
+            headers: {
+                authorization: token
+            }
+        })
+        if(response.status === 200){
+            console.log("note unpinned successfully")
+        }
+    } catch (error) {
+        console.log("error while unpinning")
+    }
+}
