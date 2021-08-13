5eba68eb7b26275d6b90b56d2da315239586b370
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:09:45 +0530

    create bgColorUtil for archive

---
+++ src/Context/utils/ArchiveNotesAxios/changebgArchivedNote.ts
@@ -0,0 +1,29 @@
+import { Dispatch } from 'react';
+import axios from "axios"
+import { ACTION } from '../../reducer/actions';
+
+export type BgColor = {
+    color:string;
+    dispatch :Dispatch<ACTION>;
+    id: number;
+    token:string
+}
+
+export async function changeArchiveNotesBg({color,id,dispatch,token}:BgColor){
+    dispatch({type:"CHANGE_ARCHIVED_NOTES_BG",payload:{colorName:color,id:id}})
+    try {
+        const response = await axios.post("http://localhost:8080/user/archiveNote/color",{
+            noteId:id,
+            newColor:color
+        },{
+            headers: {
+                authorization: token
+            }
+        })
+        if(response.status === 200){
+            console.log("color updated successfully")
+        }
+    } catch (error) {
+        console.log("error occured while updating color from others")
+    }
+}
