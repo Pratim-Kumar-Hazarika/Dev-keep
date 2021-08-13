01e631edbc84897724f2fcd8115937649af4aa9a
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-13 22:11:29 +0530

    Create getUserPinnedNotes.ts

---
+++ src/Context/utils/GetNotesFromServer/getUserPinnedNotes.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../../reducer/actions";
+
+export type GetUserNotesFromServer = {
+    dispatch :Dispatch<ACTION>;
+    token:string;
+}
+export function getUserPinnedNotesFromServer({dispatch,token}:GetUserNotesFromServer): void {
+    (async function () {
+        try {
+            const response = await axios.get(`http://localhost:8080/user/pinnedNote`,{
+                headers:{
+                    authorization:token
+                }
+            });
+            console.log("from Pinned",response.data.getUserNotes.notes)
+            const allNotes = response.data.getUserNotes.pinnedNotes.map(({images,label,title,_id,color,description}:any)=>({
+                images:images.map((image:any)=>({
+                    image:image.imageUrl,
+                    id:image._id
+                }))
+                ,
+                title:title,
+                description:description,
+                label :label,
+                id:_id ,
+                color:color,
+            }))
+            if (response.status === 200) {
+                dispatch({ type: "GET_PINNED_NOTES_FROM_SERVER", payload: { allNotes:allNotes} });            
+            }
+            return response.data.videos;
+        } catch (error) {
+           return error;
+        }
+    })();
+}
