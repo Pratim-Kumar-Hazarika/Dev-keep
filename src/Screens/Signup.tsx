9fb6b9c11255627c65dfc289fb7c7e4b60886cd1
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 17:35:08 +0530

    fix : css

--- src/Screens/Signup.tsx
+++ src/Screens/Signup.tsx
@@ -1,13 +1,13 @@
 import SignUpForm from "../Components/Signup/SignupForm";
-import {ReactComponent as HeroImage} from "../Svgs/notes.svg"
+import {ReactComponent as HeroImage} from "../Svgs/notes3.svg"
 export default function Signup() {
     return (
         <div className="login">
             <div className="login_text">
                 <div className="quote">
-                    <span className="note_quote mt_10">Relax And Take Notes</span>
+                    {/* <span className="note_quote mt_10">Relax And Take Notes</span> */}
                 </div>
-                <div className="hero_image">
+                <div className="hero_image1">
                     <HeroImage/>
                 </div>
             </div>
