8a8cc09b7b8cb3856586d9658fe8a3935b23b654
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 14:56:59 +0530

    refactor : move to label utils

--- src/Components/EditLabel/EditLabel.tsx
+++ src/Components/EditLabel/EditLabel.tsx
@@ -3,37 +3,20 @@ import {useGoogleKeep} from "../../Context/GoogleKeepProvider";
 import {CreateLabel} from "../Reusable/CreateLabel";
 import { DeleteLabel } from "../Reusable/DeleteLabel";
 import {DeleteModel} from "../DeleteModel/DeleteModel";
-import { MdiPencil } from "../Svgs/Svg";
 import { useAuth } from "../../Context/AuthProvider";
-import axios from "axios";
+import { editLabelHandler } from "../../Context/utils/Label/editLabelHandler";
+import { createLabelClickHandler } from "../../Context/utils/Label/createLabel";
 export default function EditLabel() {
     const [newLabel,setNewLabel] = useState < string > ('')
     const {showLabelModel, setShowLabelModel, state, dispatch,setShowDeleteModel,setKeepOpacity} = useGoogleKeep()
     const {token} = useAuth();
-    
-   async function createLabelClickHandler(){
-    dispatch({type:"ADD_LABEL",payload:{labelName:newLabel,id:Math.random()}})
-    setNewLabel("")
-    try {
-        const response = await axios.post("http://localhost:8080/user/labels",{
-            labelName:newLabel
-        },{
-            headers: {
-                authorization: token
-            }
-        })
-        if(response.status==200){
-            console.log("label added sucessfully")
+    async  function closeEditLabel(){
+            setShowLabelModel("hidden")
+            setKeepOpacity(false)
         }
-    } catch (error) {
-        console.log("error while creating the label")
-    }
-    }
-    function closeEditLabel(){
-        setShowLabelModel("hidden")
-        setKeepOpacity(false)
-    }
+ 
     return (
+        <>
         <div
             className="label_main"
             style={{
@@ -50,42 +33,43 @@ export default function EditLabel() {
                     onChange={(e) => setNewLabel(e.target.value)}
                     placeholder="Create new label"/>
                 <div className="icon_color">
-                   <CreateLabel onClick={createLabelClickHandler}/>
+                   <CreateLabel onClick={()=>createLabelClickHandler({dispatch,newLabel,setNewLabel,token})}/>
                 </div>
             </div>
             {state
                 .labels
-                .map((label,index) => (
+                .map(({labelName,id:labelId},index) => (
                     <div className="label_content" key={index}>
                         <div className="name_logo">
                             <div className="icon_color">
                             <DeleteLabel onClick={()=>setShowDeleteModel("visible")} />
-                          <DeleteModel id={label.id}  labelName={label.labelName}/>
+                          <DeleteModel id={labelId}  labelName={labelName}/>
                             </div>
                             <div >
                                 <input
                                     className="input"
                                     type="text"
-                                    value={label.labelName}
+                                    value={labelName}
                                     onChange={(e) => dispatch({
                                     type: "EDIT_LABLES",
                                     payload: {
                                         labelName: e.target.value,
-                                        id: label.id
+                                        id: labelId
                                     }
                                 })}/>
                             </div >
                         </div>
                         < div className="icon_color">
-                            <MdiPencil/>
+                <button className="update_btn"  onClick={() => editLabelHandler({labelId,labelName,setNewLabel,token})}>Update</button>
                         </div>
                     </div >
                 ))
             }
             <div className="border"/>
             <div className="btn_div">
-                <button className="done_btn" onClick={() => closeEditLabel()}>Done</button>
+                <button className="done_btn" onClick={() => closeEditLabel()}>Close</button>
             </div>
         </div>
+        </>
     )
 }
