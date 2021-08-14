d7a1f1aae308ac352ae43e55d80979a6ef408d0e
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 16:02:17 +0530

    fix : undefined

--- src/Context/utils/filterNoteToBeEdited.ts
+++ src/Context/utils/filterNoteToBeEdited.ts
@@ -1,7 +1,7 @@
 import { ReducerInitialState } from './../types';
 export function filterNoteToBeEdited(state: ReducerInitialState, noteId: string | any) {
-    const filterNote = state.notes.filter((note) => note.id === noteId)
-    const filterPinnedNote = state.pinnedNotes.filter((note) => note.id === noteId)
-    const filterArchiveNote = state.archive.filter((note) => note.id === noteId)
+    const filterNote = state.notes.filter((note) => note.id === noteId) || {}
+    const filterPinnedNote = state.pinnedNotes.filter((note) => note.id === noteId) || {}
+    const filterArchiveNote = state.archive.filter((note) => note.id === noteId) || {}
     return { filterNote, filterPinnedNote, filterArchiveNote }
 }
