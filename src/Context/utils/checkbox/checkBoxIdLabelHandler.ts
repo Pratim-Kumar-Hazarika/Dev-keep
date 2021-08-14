3ebdb79c8d840da4a6baf0d3146f03230dbdada8
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 16:06:09 +0530

    Create checkBoxIdLabelHandler.ts

---
+++ src/Context/utils/checkbox/checkBoxIdLabelHandler.ts
@@ -0,0 +1,9 @@
+import { Notes } from '../../types';
+export function idInLabelCheckHandler(getNote: Notes[], id: any) {
+    return getNote[0]?.label.filter((label) => label.id === id);
+}
+
+export function checkLabelInNotesHandler(getNote: Notes[], id: any) {
+    return getNote[0].label.find((label) => label.id === id);
+}
+
