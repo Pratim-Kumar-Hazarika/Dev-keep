ee38930fbecafa161f1a033d964a9ffbb144f3cf
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 16:01:09 +0530

    refactor

--- src/Components/EditNote/EditNote.tsx
+++ src/Components/EditNote/EditNote.tsx
@@ -1,44 +1,30 @@
 import axios from 'axios'
 import React, { useEffect, useRef } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import { useAuth } from '../../Context/AuthProvider'
 import { useGoogleKeep } from '../../Context/GoogleKeepProvider'
 import { colorsData } from '../../Context/reducer/colors'
 import { Image, NoteLabelTypes } from '../../Context/types'
-import { addNoteHandler } from '../../Context/utils/addNoteHandler'
-import { archiveClickHandler } from '../../Context/utils/archiveNoteClickHandler'
-import { unarchiveNote } from '../../Context/utils/ArchiveNotesAxios/unarchive'
-import { changeArchiveNotesDescription } from '../../Context/utils/DescriptionTitle/DescriptionUpdateAxios/updateArchiveDescription'
 import { editDescription } from '../../Context/utils/DescriptionTitle/DescriptionUpdateAxios/updateDescriptionDispatchHandler'
-import { changeNotesDescription } from '../../Context/utils/DescriptionTitle/DescriptionUpdateAxios/updateNoteDescription'
-import { changePinnedNotesDescription } from '../../Context/utils/DescriptionTitle/DescriptionUpdateAxios/updatePinnedNotesDescription'
 import { changeColorFromEditModel } from '../../Context/utils/EditNote/changeColorFromEditModel'
-import { deleteImageFromArchive } from '../../Context/utils/Image/ImageDeleteAxios/deleteImageFromArchive'
-import { deleteImageFromOthers } from '../../Context/utils/Image/ImageDeleteAxios/deleteImageFromOthers'
-import { deleteImageFromPinnedNotes } from '../../Context/utils/Image/ImageDeleteAxios/deleteImageFromPinned'
 import { deleteImageHandler } from '../../Context/utils/Image/ImageDeleteHandler'
-import { archiveNoteFromOthers } from '../../Context/utils/OtherNotesAxios/archiveNoteFromOthers'
-import { archivePinNote } from '../../Context/utils/PinnedNotesAxios/archivePinNote'
 import { editNoteTitle } from '../../Context/utils/DescriptionTitle/TitleUpdateAxios/editeTitleDispatchHandler'
-import { changeArchiveNotesTitle } from '../../Context/utils/DescriptionTitle/TitleUpdateAxios/updateArchiveNoteTitle'
-import { changeNotesTitle } from '../../Context/utils/DescriptionTitle/TitleUpdateAxios/updateNotesTitle'
-import { changePinnedNotesTitle } from '../../Context/utils/DescriptionTitle/TitleUpdateAxios/updatePinnedNotesTitle'
 import AddImage from '../Reusable/AddImage'
 import { ArchiveNote } from '../Reusable/ArchiveNote'
 import { ChangeColor } from '../Reusable/ChangeColor'
 import { DeleteNote } from '../Reusable/DeleteNote'
-import { DisplayImage } from '../Reusable/DisplayImage'
 import { EditNoteImage } from '../Reusable/EditNoteImage'
-import { MediumImages } from '../Reusable/MediumImages'
 import { NoteMediumImages } from '../Reusable/NoteMediumImages'
 import { PinFromModel } from '../Reusable/PinFromModel'
-import { PinNote } from '../Reusable/PinNote'
-import { PinNoteFromInput } from '../Reusable/PinNoteFromInput'
-import { ShowImage } from '../Reusable/ShowImage'
-import { SmallImages } from '../Reusable/SmallImages'
 import { VerticalDots } from '../Reusable/VerticalDots'
-import { NotoV1CrossMark } from '../Svgs/Svg'
-import { archiveNoteTitleDescriptionHandler, cardTitleDescriptionHandler, pinnedNoteTitleDescriptionHandler } from '../../Context/utils/DescriptionTitle/titleDesriptionHandlers'
+import { achiveNoteFromEditModel } from '../../Context/utils/EditNote/archiveNoteFromEditModel'
+import { UnpinNote } from '../Reusable/UnpinNote'
+import { UnarchiveNote } from '../Reusable/UnarchiveNote'
+import { deleteNoteFromEditModel } from '../../Context/utils/EditNote/deleteNoteFromEditMode'
+import { pinNoteFromEdit } from '../../Context/utils/EditNote/pinNoteFromEditMode'
+import { unpinNoteFromEdit } from '../../Context/utils/EditNote/unpinNoteFromEditModel'
+import { unarchiveNoteFromEdit } from '../../Context/utils/EditNote/unarchiveNoteFromEditModel'
+import { saveNote } from '../../Context/utils/EditNote/saveNoteFromEditModel'
 
 export type EditNoteProps = {
     title:string;
@@ -56,68 +42,16 @@ export const EditNote: React.FC<EditNoteProps> = ({title,description,id,color,fr
     const navigate = useNavigate()
     const {token} = useAuth()
     const textRef = useRef<any>()
-   async function saveNote(e:any){
-        e.preventDefault();
-        setShowEditNoteModel("hidden");
-        setKeepOpacity(false)
-        if(from ==="archive"){
-            navigate("/archive")
-          return  await archiveNoteTitleDescriptionHandler(id, description, token, title)
-        }else if(from ==="pinnedCard"){
-            navigate("/home")
-          return await pinnedNoteTitleDescriptionHandler(id, description, token, title)
-        }else if(from ==="card"){
-            navigate("/home")
-           return await cardTitleDescriptionHandler(id, description, token, title)
-        }   
-    }
-
     useEffect(()=>{
         setKeepOpacity(true)
     },[noteId])
 
-
+    function setHeight(e:any){
+        const target = e.target as HTMLTextAreaElement;
+        textRef.current.style.height = "50px";
+        textRef.current.style.height = `${target.scrollHeight}px`;
+    }
  
-    
-    function pinNote(){
-        if(from ==="card"){
-            dispatch({type:"PIN_NOTE",payload:{id}})
-        }else if(from =="pinnedCard"){
-            dispatch({type:"UNPIN_NOTE",payload:{id}})
-        }else if(from ==="archive"){
-            dispatch({type:"PIN_ARCHIVED_NOTE",payload:{id}})
-        }
-       
-    }
-  async function achiveNoteFromEditModel(){
-        if(from ==="card"){
-        await archiveNoteFromOthers({dispatch,id,token})
-        }else if(from =="pinnedCard"){
-          await archivePinNote({dispatch,id,token})
-        }else if(from ==="archive"){
-         await unarchiveNote({dispatch,id,token})
-        }
-        
-    }
-    function deleteNoteFromEditModel(){
-        if(from ==="card"){
-            dispatch({type:"DELETE_NOTE",payload:{id}})
-            
-        }else if(from =="pinnedCard"){
-            dispatch({type:"DELETE_PINNED_NOTE",payload:{id}})
-        }else if(from ==="archive"){
-            dispatch({type:"DELETE_ARCHIVED_NOTE",payload:{id}})
-        }
-        // setShowEditNoteModel("hidden");
-        // setKeepOpacity(false)
-    }
-   
- 
-  function setHeight(e:any){
-    const target = e.target as HTMLTextAreaElement;
-    textRef.current.style.height = "50px";
-    textRef.current.style.height = `${target.scrollHeight}px`;
-  }
 
     return (
         <>
@@ -143,7 +77,9 @@ export const EditNote: React.FC<EditNoteProps> = ({title,description,id,color,fr
                     <input style={{backgroundColor:color}} className="card_title_input" value={title}
                         placeholder="Title" type="text"
                         onChange={(e)=>editNoteTitle({e,from,dispatch,id})} />
-                    <PinFromModel onClick={()=>pinNote()}/>
+                    {
+                        from === "pinnedCard" ? <UnpinNote onClick={()=>unpinNoteFromEdit({from,dispatch,id,token,setShowEditNoteModel,setKeepOpacity,navigate})}/> :<PinFromModel onClick={()=>pinNoteFromEdit({from,dispatch,id,token,setShowEditNoteModel,setKeepOpacity,navigate})}/>
+                    }
                 </div>
                 <div className="card_text_box">
                     <textarea style={{backgroundColor:color}} ref={textRef}  className="text_area"
@@ -172,10 +108,13 @@ export const EditNote: React.FC<EditNoteProps> = ({title,description,id,color,fr
                             </div>
                         </div>
                         <AddImage from={from} noteId={id}/>
-                        <ArchiveNote onClick={()=>achiveNoteFromEditModel()}/>
-                            <DeleteNote onClick={deleteNoteFromEditModel}/>
+                      {
+                          from === "archive" ?  <UnarchiveNote
+                          onClick={() =>unarchiveNoteFromEdit({from,dispatch,id,token,setShowEditNoteModel,setKeepOpacity,navigate})}/> : <ArchiveNote onClick={()=>achiveNoteFromEditModel({from,dispatch,id,token,setShowEditNoteModel,setKeepOpacity,navigate})}/>
+                      }  
+                            <DeleteNote onClick={()=>deleteNoteFromEditModel({from,dispatch,id,token,setShowEditNoteModel,setKeepOpacity,navigate})}/>
                                <VerticalDots noteId={id} from={from} />
-                               <button className="addNote_btn"  type="submit" onClick={(e)=>saveNote(e)}>Save Note</button>
+                               <button className="addNote_btn"  type="submit" onClick={(e)=>saveNote({e,from,id,token,setShowEditNoteModel,setKeepOpacity,navigate,description,title})}>Save Note</button>
                     </div>
                 </div>
             </form>
