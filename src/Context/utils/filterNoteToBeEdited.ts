62a02ba41746e8b8f635e02e13a8401399a425b8
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 14:56:18 +0530

    fix : remove console log

--- src/Context/utils/filterNoteToBeEdited.ts
+++ src/Context/utils/filterNoteToBeEdited.ts
@@ -1,7 +1,6 @@
 import { ReducerInitialState } from './../types';
 export function filterNoteToBeEdited(state: ReducerInitialState, noteId: string | any) {
     const filterNote = state.notes.filter((note) => note.id === noteId)
-    console.log("filter note is",filterNote)
     const filterPinnedNote = state.pinnedNotes.filter((note) => note.id === noteId)
     const filterArchiveNote = state.archive.filter((note) => note.id === noteId)
     return { filterNote, filterPinnedNote, filterArchiveNote }
