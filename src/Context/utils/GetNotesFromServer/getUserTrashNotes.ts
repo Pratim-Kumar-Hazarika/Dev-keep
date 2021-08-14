aa65d7111f5f05e531895bf7e3337eb14e7afdd1
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 14:37:30 +0530

    Create getUserTrashNotes.ts

---
+++ src/Context/utils/GetNotesFromServer/getUserTrashNotes.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../../reducer/actions";
+
+export type GetUserNotesFromServer = {
+    dispatch :Dispatch<ACTION>;
+    token:string;
+}
+export function trashNotesFromServer({dispatch,token}:GetUserNotesFromServer): void {
+    (async function () {
+        try {
+            const response = await axios.get(`http://localhost:8080/user/trash`,{
+                headers:{
+                    authorization:token
+                }
+            });
+            console.log("trash notes",response)
+            const allTrashNotes = response.data.getUserNotes.trashNotes.map(({images,label,title,_id,color,description}:any)=>({
+                images:images.map((image:any)=>({
+                    image:image.imageUrl,
+                    id:image._id
+                }))
+                ,
+                title:title,
+                description:description,
+                label :label.map((label:any)=>(
+                    {
+                      labelName:label.labelName,
+                      id:label._id 
+                    }
+                )),
+                id:_id ,
+                color:color,
+            }))
+            if (response.status === 200) {
+               return dispatch({ type: "GET_TRASH_NOTES_FROM_SERVER", payload: { allNotes:allTrashNotes} });            
+            }
+        } catch (error) {
+           return error;
+        }
+    })();
+}
