84c88aac0e61c406232a7e32d60b2ab5421b05a1
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-15 14:36:56 +0530

    update : sidebar state

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,14 +2,24 @@ import "./Header.css";
 import {Link} from "react-router-dom"
 import { EmojioneV1NotePage } from "../Svgs/Svg";
 import { useAuth } from "../../Context/AuthProvider";
+import { useGoogleKeep } from "../../Context/GoogleKeepProvider";
+import { NotoV1CrossedSwords, OpenmojiHamburgerMenu } from "../../Svgs/Svgs";
 
 function Header() {
+    const {setSidebar,sidebar} = useGoogleKeep()
     const {signoutHandler} = useAuth()
+
     return (
         <div className="header">
+            <div className="hamburger">
+                {
+                    sidebar ?<NotoV1CrossedSwords onClick={()=>setSidebar(!sidebar)}/> : <OpenmojiHamburgerMenu onClick={()=>setSidebar(!sidebar)}/>
+                }
+                
+            </div>
             <div className="logo_text ">
-            <EmojioneV1NotePage/> 
-            <span className="keep">DevKeep</span>
+            <EmojioneV1NotePage className="logo_icon"/> 
+            <span className="keep keep_text">DevKeep</span>
             </div>
             <div className="flex">
                 <Link to="/profile">
