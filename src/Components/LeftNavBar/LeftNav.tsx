ab75036a3b2928ec31b0f98d123758cfe7f17546
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-15 14:37:08 +0530

    fix: css

--- src/Components/LeftNavBar/LeftNav.tsx
+++ src/Components/LeftNavBar/LeftNav.tsx
@@ -4,34 +4,41 @@ import {Link} from "react-router-dom"
 import { useGoogleKeep } from '../../Context/GoogleKeepProvider'
 import { NotoV1Wastebasket, OpenmojiArchive, TwemojiLabel } from '../Svgs/Svg'
 
+
 export default function LeftNav() {
-    const {setShowLabelModel,state,setKeepOpacity} = useGoogleKeep();
+    const {setShowLabelModel,state,setKeepOpacity,sidebar} = useGoogleKeep();
     function labelHandler(){
         setShowLabelModel("visible")
         setKeepOpacity(true)
     }
     return (
-        <div className="left_nav" >
+        <div className={sidebar ? "left_nav active" : "left_nav"} >
+
          <Link to="/home" className="link" >
                 <div className="left_nav_contents notes">
                         <FxemojiLightbulb className="svg"/>
-                        <span className="left_nav_icons_text">Notes</span>
+                        <span className="left_nav_icons_text" >Notes</span>
+                        <span className={sidebar ? "sidebartext active" : "sidebartext"}>Notes</span>
                 </div>
         </Link>
+     
         <Link to="/archive"  className="link">
             <div className="left_nav_contents archive">
                     <OpenmojiArchive className="svg"/>
                     <span className="left_nav_icons_text">Archive</span>
+                    <span className={sidebar ? "sidebartext active" : "sidebartext"}>Archive</span>
             </div>
         </Link>
             <div className="left_nav_contents edit_labels" onClick={labelHandler}>
                 <EmojionePencil className="svg"/>
                 <span className="left_nav_icons_text" >Edit Lables</span>
+                <span className={sidebar ? "sidebartext active" : "sidebartext"}>Notes</span>
             </div>
         <Link to="/trash"  className="link">
             <div className="left_nav_contents trash">
                     <NotoV1Wastebasket className="svg"/>
                     <span className="left_nav_icons_text">Trash</span>
+                    <span className={sidebar ? "sidebartext active" : "sidebartext"}>Notes</span>
             </div>
         </Link>
         {
@@ -40,6 +47,7 @@ export default function LeftNav() {
                 <div className="left_nav_contents trash" >
                         <TwemojiLabel className="svg"/>
                         <span className="left_nav_icons_text">{label.labelName}</span>
+                        <span className={sidebar ? "sidebartext active" : "sidebartext"}>{label.labelName}</span>
                 </div>
             </Link>
             ))
