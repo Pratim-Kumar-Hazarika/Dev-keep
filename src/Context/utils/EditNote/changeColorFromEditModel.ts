ea7eed1b958056304bdeb60b6ad921329f542035
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-16 19:55:02 +0530

    refactor : remove dispatch

--- src/Context/utils/EditNote/changeColorFromEditModel.ts
+++ src/Context/utils/EditNote/changeColorFromEditModel.ts
@@ -6,13 +6,10 @@ import { changePinnedNotesBg } from '../PinnedNotesAxios/changeBgPinnedNote';
 
 export async function changeColorFromEditModel(color:any,dispatch:Dispatch<ACTION>,id:any,from:string,token:string){
     if(from ==="card"){
-        dispatch({type:"CHANGE_OTHER_NOTES_BG",payload:{colorName:color,id:id}})
          await changeOtherNotesBg({color,id,dispatch,token})
     }else if(from ==="pinnedCard"){
-        dispatch({type:"CHANGE_PINNED_NOTES_BG",payload:{colorName:color,id:id}})
        await changePinnedNotesBg({color,id,dispatch,token})
     }else if(from ==="archive"){
-        dispatch({type:"CHANGE_ARCHIVED_NOTES_BG",payload:{colorName:color,id:id}})
        await changeArchiveNotesBg({color,id,dispatch,token})
     }
     
